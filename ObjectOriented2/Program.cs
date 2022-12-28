460f438656648a6d45fc0736db9bc56f0e8215e6
khas <khas@FABLAB05>
2022-12-28 15:11:11 +0300

    Add project files.

---
+++ ObjectOriented2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ObjectOriented2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            insan a= new insan();
+            a.Tanis();
+
+        }
+    }
+}
---
+++ ObjectOriented2/insan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOriented2
+{
+    internal class insan
+    {
+        public insan() {
+            ad = "Esra";
+            soyad = "UÇAR";
+            yas = 28;
+            kilo = 60;
+            boy= 1.60;
+        }
+
+        public void Tanis() {
+            Console.Write("Ben: {0} {1}... Boyum {2}, Kilom {3}, Yasim {4}",ad,soyad,boy,kilo,yas);           
+        }
+
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public double boy { get; set; }
+        public int kilo { get; set; }
+        public int yas { get; set; }
+
+
+
+
+
+    }
+}
+
